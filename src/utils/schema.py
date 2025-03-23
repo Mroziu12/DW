ffb3d05e924ccb0e8b94f1afb4233037b6b0dcac
Mateusz Mroziu <m.mroziu@interia.pl>
2025-03-23 13:51:44 +0100

    ClientID naprawiony
    Signed-off-by: Mateusz Mroziu <m.mroziu@interia.pl>

--- src/utils/schema.py
+++ src/utils/schema.py
@@ -4,9 +4,9 @@ DATABASE_SCHEMA = {
     "Branch": ["BranchID", "Localisation"],
     "Employee": ["EmployeeID", "Name", "Surname", "Role", "BranchID"],
     "Customer": ["Pesel", "Name", "Surname", "Date_Of_Birth", "Phone_Number", "Email", "Address"],
-    "Account": ["Account_Number", "Type", "Open_Date", "Balance", "Customer_ID"],
+    "Account": ["Account_Number", "Type", "Open_Date", "Balance", "CustomerID"],
     "Transactions": ["TransactionID", "Amount", "Date"],
     "Account_Transaction": ["TransactionID", "Sender_AccountID", "Receiver_AccountID"],
-    "Loan": ["LoanID", "Type", "Amount", "Interest_Rate", "Approval_Date", "Final_Repayment_Date", "Date_Of_Credit_Closing", "Status", "Customer_ID", "Employee_ID"],
+    "Loan": ["LoanID", "Type", "Amount", "Interest_Rate", "Approval_Date", "Final_Repayment_Date", "Date_Of_Credit_Closing", "Status", "CustomerID", "EmployeeID"],
     "Loan_Payment": ["PaymentID", "Amount", "Date", "Status", "Date_of_expected_payment", "LoanID"]
 }
